b7d8b6c1d9448ac6395178718c3ba560c98fb4ea
Thomas <pachown@gmail.com>
2021-03-05 16:06:12 -0700

    Made css tweaks to footer to have icons space properly

--- website/components/Footer.js
+++ website/components/Footer.js
@@ -5,23 +5,21 @@ import { SocialIcon } from 'react-social-icons'
 const Footer = ({ children }) => {
   return (
     <div>
-      <footer className={styles.footer}>
-        <h3>Reach Me:</h3>
-        <a>
-          <SocialIcon url="https://www.linkedin.com/in/thomas-mcnutt-97526588/" />
-        </a>
-        <a>
-          <SocialIcon url="https://github.com/pachown" />
-        </a>
+      <div className={styles.footer}>
+        <h3>Reach Me @:</h3>
+        <SocialIcon url="https://www.linkedin.com/in/thomas-mcnutt-97526588/" />
+        <a></a>
+        <SocialIcon url="https://github.com/pachown" />
+
         <br />
         <a>
           Phone: 720-840-9658
         </a>
         <br />
-        <a>
-          Email: <a href="mailto:Pachown@gmail.com">Email Me</a>
-        </a>
-      </footer>
+        <a href="mailto:Pachown@gmail.com">
+          Email Me
+          </a>
+      </div>
     </div>
   )
 }
