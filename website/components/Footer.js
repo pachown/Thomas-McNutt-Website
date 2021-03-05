6ef809bc51493f11d006bb9d25e5351163cf935b
Thomas <pachown@gmail.com>
2021-03-05 14:59:12 -0700

    Changed Footer to have attractive proportions

--- website/components/Footer.js
+++ website/components/Footer.js
@@ -1,17 +1,26 @@
 import styles from '../styles/Layout.module.css'
 
-import {SocialIcon} from 'react-social-icons'
+import { SocialIcon } from 'react-social-icons'
 
 const Footer = ({ children }) => {
   return (
     <div>
       <footer className={styles.footer}>
-      Reach Me:
-        <SocialIcon url="https://www.linkedin.com/in/thomas-mcnutt-97526588/"/>
-        <SocialIcon url="https://github.com/pachown"/>
-        Phone: 720-840-9658
-        <br></br>
-        Email: Pachown@gmail.com
+        <h3>Reach Me:</h3>
+        <a>
+          <SocialIcon url="https://www.linkedin.com/in/thomas-mcnutt-97526588/" />
+        </a>
+        <a>
+          <SocialIcon url="https://github.com/pachown" />
+        </a>
+        <br />
+        <a>
+          Phone: 720-840-9658
+        </a>
+        <br />
+        <a>
+          Email: <a href="mailto:Pachown@gmail.com">Email Me</a>
+        </a>
       </footer>
     </div>
   )
