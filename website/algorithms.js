79ab683adfe3a3f3a3d9305b310a02ce4be0d6cb
Thomas R McNutt <pachown@gmail.com>
2021-04-01 14:25:50 -0600

    Added LeetCode #292 Nim Game

--- website/algorithms.js
+++ website/algorithms.js
@@ -267,7 +267,7 @@ Memory Usage: 40.4 MB, less than 74.28% of JavaScript online submissions for Del
 };`,
  `Runtime: 100 ms, faster than 55.45% of JavaScript online submissions for Remove Linked List Elements. \n
 Memory Usage: 44.5 MB, less than 5.67% of JavaScript online submissions for Remove Linked List Elements.`],
-'217. Contains Duplicate': [`Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.`,
+'LeetCode #217 Contains Duplicate': [`Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.`,
 `var containsDuplicate = function(nums) {
   let uniqs = {};
   for(var i = 0; i < nums.length; i++) {
@@ -280,6 +280,18 @@ Memory Usage: 44.5 MB, less than 5.67% of JavaScript online submissions for Remo
   return false;
 };`,
 `Runtime: 92 ms, faster than 56.36% of JavaScript online submissions for Contains Duplicate.\n
-Memory Usage: 47.5 MB, less than 11.06% of JavaScript online submissions for Contains Duplicate.`]
+Memory Usage: 47.5 MB, less than 11.06% of JavaScript online submissions for Contains Duplicate.`],
+  'Leetcode #292 Nim Game': [`You are playing the following Nim Game with your friend:
+
+Initially, there is a heap of stones on the table.
+You and your friend will alternate taking turns, and you go first.
+On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+The one who removes the last stone is the winner.
+Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.`,
+`var canWinNim = function(n) {
+    return n%4 !== 0
+};`,
+`Runtime: 68 ms, faster than 95.36% of JavaScript online submissions for Nim Game.\n
+Memory Usage: 38.4 MB, less than 44.37% of JavaScript online submissions for Nim Game.`]
 };
 module.exports = algos;
