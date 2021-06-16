d150688d000ad8438d625db47d56537586db34b1
Thomas <pachown@gmail.com>
2021-06-16 12:01:32 -0600

    Changed algo css backgrounds, borders, and made it mobile responsive

--- website/components/Algorithms.js
+++ website/components/Algorithms.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Layout.module.css'
+import styles from '../styles/Algos.module.css'
 import algoData from '../algorithms.js'
 import React, { useState, useEffect } from 'react'
 import PrismAsyncLight from 'react-syntax-highlighter'
@@ -12,20 +12,25 @@ const Algorithms = ({ children }) => {
 
   return (
     <div className={styles.algos}>
-        <h2 className={styles.blog}><em>Welcome to my algorithm blog!</em></h2>
-        <h2 className={styles.blog2}><em> First the question, then MY solution, and then my results according to the website</em>
+        <h1 className={styles.blog}><em>Welcome to my algorithm blog!</em></h1>
+        <h2 className={styles.blog2}><em> First the question, then my solution, and then my results according to the website</em>
           </h2>
       {data && (
           Object.keys(data).map((algo) =>
             <div className={styles.algoaday}>
-              <div className={styles.date}>Problem: {algo}</div>
-              <div className={styles.date}>Language: {data[algo][3]}</div>
+              <div className={styles.problem}>Problem: {algo}</div>
+              <div className={styles.language}>Language: {data[algo][3]}</div>
               <div className={styles.algoinfo}><em>Prompt:</em> {data[algo][0]}</div>
               <PrismAsyncLight
               language="javascript">
                 {data[algo][1]}
               </PrismAsyncLight>
               <div className={styles.algoinfo}>Result: {data[algo][2]}</div>
+              <div className={styles.container1}>
+              <div className={styles.chevron}></div>
+              <div className={styles.chevron}></div>
+              <div className={styles.chevron}></div>
+        </div>
             </div>
           )
       )}
