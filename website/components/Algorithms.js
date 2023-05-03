097603b40c45517b17b37795617c80fda0dc385a
Thomas R McNutt <pachown@gmail.com>
2023-05-03 11:47:42 -0600

    Commented out algos for now

--- website/components/Algorithms.js
+++ website/components/Algorithms.js
@@ -15,25 +15,25 @@ const Algorithms = ({ children }) => {
       <h1 className={styles.blog}><em>Welcome to my algorithm blog!</em></h1>
       <h2 className={styles.blog2}><em> First the question, then my solution, and then my results according to the website</em>
       </h2>
-      {data && (
-        Object.keys(data).map((algo) =>
-          <div className={styles.algoaday}>
-            <div className={styles.problem}>Problem: {algo}</div>
-            <div className={styles.language}>Language: {data[algo][3]}</div>
-            <div className={styles.algoinfo}><em>Prompt:</em> {data[algo][0]}</div>
-            <PrismAsyncLight
-              language="javascript">
-              {data[algo][1]}
-            </PrismAsyncLight>
-            <div className={styles.algoinfo}>Result: {data[algo][2]}</div>
-            <div className={styles.container1}>
-              <div className={styles.chevron}></div>
-              <div className={styles.chevron}></div>
-              <div className={styles.chevron}></div>
-            </div>
-          </div>
-        )
-      )}
+      //{data && (
+        //Object.keys(data).map((algo) =>
+          //<div className={styles.algoaday}>
+            //<div className={styles.problem}>Problem: {algo}</div>
+            //<div className={styles.language}>Language: {data[algo][3]}</div>
+            //<div className={styles.algoinfo}><em>Prompt:</em> {data[algo][0]}</div>
+            //<PrismAsyncLight
+             // language="javascript">
+              //{data[algo][1]}
+            //</PrismAsyncLight>
+            //<div className={styles.algoinfo}>Result: {data[algo][2]}</div>
+            //<div className={styles.container1}>
+             // <div className={styles.chevron}></div>
+              //<div className={styles.chevron}></div>
+              //<div className={styles.chevron}></div>
+            //</div>
+          //</div>
+       // )
+      //)}
     </div>
   )
 };
