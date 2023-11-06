b6c4c184539a62e43cab7ae368581674345627d7
Thomas R McNutt <pachown@gmail.com>
2023-11-06 16:19:41 -0700

    Next Update

--- website/pages/projects.js
+++ website/pages/projects.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Styles from '../styles/Projects.module.css'
-import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Projects() {
   return (
@@ -14,20 +14,27 @@ export default function Projects() {
       <main className={Styles.main}>
         <h1 className={Styles.title}>Projects</h1>
 
-        <article className={Styles.project1}>
-          <h2 className={Styles.p1}>Trip Advisor Map Component</h2>
+        <article className={Styles.project}>
+          <h2 className={Styles.projectTitle}>Trip Advisor Map Component</h2>
           <div className={Styles.container}>
             <ul className={Styles.bullets}>
               <li>Interactive Google Map</li>
               <li>Search Query Populated Fields</li>
               <li>Dozens of Modular Components</li>
             </ul>
-            <img className={Styles.project1Img}></img>
+            <Image
+            src={require("../public/TripAdvisor.jpeg")}
+            width={500}
+            height={200}
+            alt="TripAdvisor lookalike Website"
+            loading="lazy"
+            ></Image>
+            {/* <img src={require('../public/TripAdvisor.jpeg')} loading="lazy"></img> */}
           </div>
         </article>
 
-        <article className={Styles.project3}>
-          <h2 className={Styles.p3}>Video Poker Application</h2>
+        <article className={Styles.project}>
+          <h2 className={Styles.projectTitle}>Video Poker Application</h2>
           <div className={Styles.container}>
             <ul className={Styles.bullets}>
               <li>Classic Video Poker</li>
@@ -35,20 +42,24 @@ export default function Projects() {
               <li>Fun For All Ages</li>
               <li>Playable <a href="/videoPoker" className="menu-item">Here</a></li>
             </ul>
-            <img className={Styles.project3Img}></img>
+            <img></img>
           </div>
         </article>
 
-        <article className={Styles.project2}>
-          <h2 className={Styles.p2}>Slot Machine Application</h2>
+        <article className={Styles.project}>
+          <h2 className={Styles.projectTitle}>Slot Machine Application</h2>
           <div className={Styles.container}>
             <ul className={Styles.bullets}>
               <li>Slot Machine With Control Over Payout Odds</li>
               <li>Slot Machine Owner Simulator</li>
               <li>Unique Customer Generation Mechanics</li>
             </ul>
-            <img className={Styles.project2Img}></img>
+            <img></img>
           </div>
+        </article>
+        <article className={Styles.project}>
+          <img ></img>
+          <p>Project 4</p>
         </article>
         {/* <article className={Styles.project4}>
           <img className={Styles.project4Img}></img>
